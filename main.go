74713fd4505417868fedeba6dfd168dafff2591d
Fiddler25 <rager.mka.ti@icloud.com>
2023-03-20 07:22:53 +0900

    setup & go mod tidy

--- main.go
+++ main.go
@@ -1,7 +1,64 @@
 package main
 
-import "fmt"
+import (
+	"clean-architecture-sample/product"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/go-kit/kit/log"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+const defaultPort = "8080"
 
 func main() {
-	fmt.Println("Hello, World!")
+	var httpAddr = flag.String("http.addr", ":"+defaultPort, "HTTP listen address")
+
+	flag.Parse()
+
+	var logger log.Logger
+	logger = log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
+	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+
+	ps := product.NewService()
+	ps = product.NewLoggingService(log.With(logger, "component", "product"), ps)
+
+	httpLogger := log.With(logger, "component", "http")
+
+	mux := http.NewServeMux()
+	mux.Handle("/v1/", product.MakeHandler(ps, httpLogger))
+
+	http.Handle("/", accessControl(mux))
+	http.Handle("/metrics", promhttp.Handler())
+
+	errs := make(chan error, 2)
+	go func() {
+		logger.Log("transport", "http", "address", *httpAddr, "msg", "listening")
+		errs <- http.ListenAndServe(*httpAddr, nil)
+	}()
+	go func() {
+		c := make(chan os.Signal)
+		signal.Notify(c, syscall.SIGINT)
+		errs <- fmt.Errorf("%s", <-c)
+	}()
+
+	logger.Log("terminated", <-errs)
+}
+
+func accessControl(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type")
+
+		if r.Method == "OPTIONS" {
+			return
+		}
+
+		h.ServeHTTP(w, r)
+	})
 }
