2d14c82fa2ba1132904c78a30e4a4fcbc2b8751c
Fiddler25 <rager.mka.ti@icloud.com>
2023-04-08 09:03:24 +0900

    Convertメソッド実装

--- pkg/external/exchangerate/exchange_rate.go
+++ pkg/external/exchangerate/exchange_rate.go
@@ -1,9 +1,12 @@
 package exchangerate
 
 import (
-	"fmt"
+	"encoding/json"
+	"net/http"
 	"net/url"
 )
+
+const defaultCurrencyCode = "JPY"
 
 type ExchangeRate struct {
 	apiKey  string
@@ -22,8 +25,50 @@ func New(baseURL, apiKey string) (*ExchangeRate, error) {
 	}, nil
 }
 
-func (er ExchangeRate) Convert(currencyCode string) {
+type (
+	convertResponse struct {
+		Success bool    `json:"success"`
+		Result  float64 `json:"result"`
+		Date    string  `json:"date"`
+		Query   query   `json:"query"`
+		Info    info    `json:"info"`
+	}
+
+	query struct {
+		To     string `json:"to"`
+		From   string `json:"from"`
+		Amount int    `json:"amount"`
+	}
+
+	info struct {
+		Rate      float64 `json:"rate"`
+		Timestamp int     `json:"timestamp"`
+	}
+)
+
+func (er ExchangeRate) Convert(currencyCode string) (float64, error) {
 	u := er.baseURL.JoinPath("exchangerates_data", "convert")
 
-	fmt.Println(u.String())
+	v := url.Values{}
+	v.Add("to", defaultCurrencyCode)
+	v.Add("from", currencyCode)
+	v.Add("amount", "1")
+
+	u.RawQuery = v.Encode()
+
+	req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+	if err != nil {
+		return 0, err
+	}
+	req.Header.Set("apikey", er.apiKey)
+
+	resp, err := http.DefaultClient.Do(req)
+	defer resp.Body.Close()
+
+	var r convertResponse
+	if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
+		return 0, err
+	}
+
+	return r.Info.Rate, nil
 }
