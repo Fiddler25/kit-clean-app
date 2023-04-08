5230ae66695201628afac36ba12a98b42b738e07
Fiddler25 <rager.mka.ti@icloud.com>
2023-04-08 16:16:11 +0900

    エラーハンドリング

--- pkg/external/exchangerate/exchange_rate.go
+++ pkg/external/exchangerate/exchange_rate.go
@@ -2,6 +2,7 @@ package exchangerate
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/url"
 )
@@ -46,6 +47,10 @@ type (
 	}
 )
 
+type exchangeRateException struct {
+	Message string `json:"message"`
+}
+
 func (a API) Convert(currencyCode string) (float64, error) {
 	u := a.baseURL.JoinPath("exchangerates_data", "convert")
 
@@ -63,7 +68,18 @@ func (a API) Convert(currencyCode string) (float64, error) {
 	req.Header.Set("apikey", a.apiKey)
 
 	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return 0, err
+	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		var e exchangeRateException
+		if err := json.NewDecoder(resp.Body).Decode(&e); err != nil {
+			return 0, err
+		}
+		return 0, fmt.Errorf("convert error: status code=%d, message=%s", resp.StatusCode, e.Message)
+	}
 
 	var r convertResponse
 	if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
